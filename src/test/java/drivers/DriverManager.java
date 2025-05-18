69cc7cdc6cbdd1de2c4a8876880329173c04dc6d
Jefferson <jsrs2108@gmail.com>
2025-05-18 14:12:28 -0300

    Driver manager updated

--- src/test/java/drivers/DriverManager.java
+++ src/test/java/drivers/DriverManager.java
@@ -3,15 +3,19 @@ package drivers;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverManager {
 
     private static WebDriver driver;
 
     public static WebDriver initializeDriver() {
-        WebDriverManager.chromedriver().setup(); // 👈 Esto descarga automáticamente el driver
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromium");
 
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         return driver;
     }
